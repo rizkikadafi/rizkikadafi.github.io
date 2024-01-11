6bb629701215acef594567a560241887634e5b44
rizkikadafi <rizkikadafi11@gmail.com>
2024-01-11 23:12:44 +0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: [
     './*.{html, md, markdown}',
     './_posts/*.{html, md, markdown}',
-    './_layouts/**/*.{md, markdown}',
+    './_layouts/**/*.{html, md, markdown}',
     './_includes/**/*.{html, md, markdown}',
     './_drafts/**/*.{html, md, markdown}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero1': "url('/assets/image/hero1.jpg')",
+        'hero2': "url('/assets/image/hero2.jpg')",
+      }
+    },
   },
   plugins: [],
 }
