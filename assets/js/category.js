f19a1f231b7e22b4ff613bd2b1a778540c0d1f55
rizkikadafi <rizkikadafi11@gmail.com>
2024-04-08 16:45:45 +0700

    update tags page

--- assets/js/category.js
+++ assets/js/category.js
@@ -6,20 +6,21 @@ const arrayCategoriesBtn = Array.from(categoriesBtn.children)
 arrayCategories[0].classList.add("flex")
 arrayCategories[0].classList.remove("hidden")
 arrayCategoriesBtn[0].classList.add("border-[1px]", "border-slate-300")
-let prevActiveElement = arrayCategories[0]
-let prevActiveBtnElement = arrayCategoriesBtn[0]
+
+let prevActiveCategory = arrayCategories[0]
+let prevActiveBtnCategory = arrayCategoriesBtn[0]
 
 arrayCategoriesBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     id = btn.getAttribute('data-category')
     const targetElement = arrayCategories.find(elm => elm.id === id);
 
-    prevActiveBtnElement.classList.remove("border-[1px]", "border-slate-300")
-    prevActiveElement.classList.remove("flex")
-    prevActiveElement.classList.add("hidden")
+    prevActiveBtnCategory.classList.remove("border-[1px]", "border-slate-300")
+    prevActiveCategory.classList.remove("flex")
+    prevActiveCategory.classList.add("hidden")
 
-    prevActiveElement = targetElement
-    prevActiveBtnElement = btn
+    prevActiveCategory = targetElement
+    prevActiveBtnCategory = btn
 
     btn.classList.add("border-[1px]", "border-slate-300")
     targetElement.classList.add("flex")
