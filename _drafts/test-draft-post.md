bf0d024945aa62555618b45a9b02508c4ffd76fc
rizkikadafi <rizkikadafi11@gmail.com>
2024-04-14 22:35:09 +0700

    add page for all projects and refactoring code

--- _drafts/test-draft-post.md
+++ _drafts/test-draft-post.md
@@ -13,3 +13,6 @@ distinguishing them from dessert bananas. The fruit is variable in size,
 color, and firmness, but is usually elongated and curved, with soft
 flesh rich in starch covered with a rind, which may be green, yellow,
 red, purple, or brown when ripe.
+
+| A | B | C |
+| rizki | kadafi | John |
--- assets/js/category.js
+++ assets/js/category.js
@@ -3,12 +3,25 @@ const categoriesBtn = document.getElementById('categories-btn')
 const arrayCategories = Array.from(categories.children)
 const arrayCategoriesBtn = Array.from(categoriesBtn.children)
 
-arrayCategories[0].classList.add("flex")
-arrayCategories[0].classList.remove("hidden")
-arrayCategoriesBtn[0].classList.add("border-[1px]", "border-slate-300")
+const urlParams = new URLSearchParams(window.location.search);
+const categoryParam = urlParams.get('category');
 
 let prevActiveCategory = arrayCategories[0]
 let prevActiveBtnCategory = arrayCategoriesBtn[0]
+
+if (categoryParam !== null) {
+  const targetElement = arrayCategories.find(elm => elm.id === categoryParam);
+  const targetBtn = arrayCategoriesBtn.find(elm => elm.getAttribute('data-category') === categoryParam);
+  targetBtn.classList.add("flex")
+  targetElement.classList.remove("hidden")
+  targetBtn.classList.add("border-[1px]", "border-slate-300")
+  prevActiveCategory = targetElement
+  prevActiveBtnCategory = targetBtn
+} else {
+  arrayCategories[0].classList.add("flex")
+  arrayCategories[0].classList.remove("hidden")
+  arrayCategoriesBtn[0].classList.add("border-[1px]", "border-slate-300")
+}
 
 arrayCategoriesBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
